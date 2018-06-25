34c49e16822c67b7cfc7ea1cee2287fec14d311d
tae-lim <taelim92@gmail.com>
2018-06-24 20:38:39 -0700

    reject function passed

--- src/underbar.js
+++ src/underbar.js
@@ -95,6 +95,15 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var notPassed = [];
+
+    _.each(collection, function(item) {
+      if (!test(item)) {
+        notPassed.push(item);
+      }
+    });
+
+    return notPassed;
   };
 
   // Produce a duplicate-free version of the array.
